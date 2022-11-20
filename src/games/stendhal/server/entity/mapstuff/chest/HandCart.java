c03229e6345b9c1972d87b78fd7cefc4a5f493d2
y40034sl <shaun.legg@student.manchester.ac.uk>
2022-11-20 16:11:57 +0000

    Make tests to check if hand cart can be used as a chest
    Make method stubs for said tests

--- src/games/stendhal/server/entity/mapstuff/chest/HandCart.java
+++ src/games/stendhal/server/entity/mapstuff/chest/HandCart.java
@@ -1,10 +1,28 @@
 package games.stendhal.server.entity.mapstuff.chest;
 
+import games.stendhal.server.entity.RPEntity;
+
 public class HandCart {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public boolean onUsed(final RPEntity user) {
+		return false;
+	}
+	
+	public void open() {
+		
+	}
+	
+	public void close() {
+		
+	}
+	
+	public boolean isOpen() {
+		return false;
 	}
 
 }
--- tests/games/stendhal/server/entity/mapstuff/chest/HandCartTest.java
+++ tests/games/stendhal/server/entity/mapstuff/chest/HandCartTest.java
@@ -4,11 +4,56 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class HandCartTest {
+import games.stendhal.server.entity.RPEntity;
+import games.stendhal.server.entity.item.Corpse;
 
+public class HandCartTest {
+	
+	/**
+	 * Tests for open.
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public final void testOpen() {
+		final HandCart hc = new HandCart();
+		assertFalse(hc.isOpen());
+		hc.open();
+
+		assertTrue(hc.isOpen());
+		hc.close();
+		assertFalse(hc.isOpen());
 	}
 
+	/**
+	 * Tests for onUsed.
+	 */
+	@Test
+	public final void testOnUsed() {
+		final HandCart hc = new HandCart();
+		assertFalse(hc.isOpen());
+		hc.onUsed(new RPEntity() {
+
+			@Override
+			protected void dropItemsOn(final Corpse corpse) {
+			}
+
+			@Override
+			public void logic() {
+
+			}
+		});
+
+		assertTrue(hc.isOpen());
+		hc.onUsed(new RPEntity() {
+
+			@Override
+			protected void dropItemsOn(final Corpse corpse) {
+			}
+
+			@Override
+			public void logic() {
+
+			}
+		});
+		assertFalse(hc.isOpen());
+	}
 }
