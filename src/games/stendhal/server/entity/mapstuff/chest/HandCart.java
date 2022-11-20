11e22fbf324bd369443025265626be1ad0f59f81
y40034sl <shaun.legg@student.manchester.ac.uk>
2022-11-20 15:30:50 +0000

    Create empty class and test case for HandCart

---
+++ src/games/stendhal/server/entity/mapstuff/chest/HandCart.java
@@ -0,0 +1,10 @@
+package games.stendhal.server.entity.mapstuff.chest;
+
+public class HandCart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ tests/games/stendhal/server/entity/mapstuff/chest/HandCartTest.java
@@ -0,0 +1,14 @@
+package games.stendhal.server.entity.mapstuff.chest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HandCartTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
