29d0bac6f81b0dcc5786033d80ff0d17a23f47a5
r21857jl <joe.loach@student.manchester.ac.uk>
2022-11-24 12:15:31 +0000

    Implement addSoil method for Shovel
    Adds the addSoil method meaning that the shovel can now place soil properly.

--- src/games/stendhal/server/entity/item/Shovel.java
+++ src/games/stendhal/server/entity/item/Shovel.java
@@ -15,10 +15,11 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 
+import games.stendhal.server.entity.Entity;
+import games.stendhal.server.entity.mapstuff.area.Allotment;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.rule.EntityManager;
-import games.stendhal.server.entity.Entity;
 import games.stendhal.server.entity.RPEntity;
 import games.stendhal.server.entity.mapstuff.area.FertileGround;
 import games.stendhal.server.entity.player.Player;
@@ -93,7 +94,14 @@ public class Shovel extends AreaUseItem {
 	}
 
 	public boolean addSoil(final StendhalRPZone zone, final int x, final int y) {
-		return false;
+		if (isFertileSoilAt(zone, x, y)) {
+			return false;
+		}
+		final Allotment al = new Allotment();
+		al.setPosition(x, y);
+		al.setSize(1, 1);
+		zone.add(al);
+		return true;
 	}
 
 	private boolean isFertileSoilAt(final StendhalRPZone zone, final int x, final int y) {
