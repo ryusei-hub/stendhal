23ef030563f4258beaa8683658848c0f1a48e6c3
Luke Durband <luke.durband@student.manchester.ac.uk>
2022-11-24 10:46:38 +0000

    Added two tests to ShovelTest
    Added basic test to the shovel test file that fails instantly

---
+++ tests/games/stendhal/server/entity/item/ShovelTest.java
@@ -0,0 +1,28 @@
+package games.stendhal.server.entity.item;
+
+import static org.junit.Assert.fail;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import games.stendhal.server.maps.MockStendlRPWorld;
+
+
+public class ShovelTest {
+    @BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		MockStendlRPWorld.get();
+	}
+
+    @Test 
+    public void ShovelRemoveTest()
+    {
+        fail();
+    }
+
+    @Test
+    public void ShovelFertileTest()
+    {
+        fail();
+    }
+}
