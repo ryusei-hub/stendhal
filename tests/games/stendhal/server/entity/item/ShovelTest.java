5db8250ded30fff322a81602c99a1df485655451
r21857jl <joe.loach@student.manchester.ac.uk>
2022-11-24 11:24:07 +0000

    Run formatter.

--- tests/games/stendhal/server/entity/item/ShovelTest.java
+++ tests/games/stendhal/server/entity/item/ShovelTest.java
@@ -10,31 +10,29 @@ import org.junit.Test;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.maps.MockStendlRPWorld;
 
-
 public class ShovelTest {
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
         MockStendlRPWorld.get();
     }
 
-    @Test 
-    public void ShovelRemoveTest()
-    {
+    @Test
+    public void ShovelRemoveTest() {
         final StendhalRPZone zone = new StendhalRPZone("zone");
         Shovel s = new Shovel("shovel", "tool", "shovel", null);
         // add soil to the world at 0,0
         s.addSoil(zone, 0, 0);
         // Soil should be able to be removed when there's soil already there.
         assertTrue(s.removeSoil(zone, 0, 0));
-        // shouldn't be able to remove it a second time, because there should'nt be any soil
+        // shouldn't be able to remove it a second time, because there should'nt be any
+        // soil
         assertFalse(s.removeSoil(zone, 0, 0));
         // removing a soil at a position that never had any soil should also fail
         assertFalse(s.removeSoil(zone, 1, 1));
     }
 
     @Test
-    public void ShovelFertileTest()
-    {
+    public void ShovelFertileTest() {
         fail();
     }
 }
