dc403bbf3d00f51e58b9dd25353d8f30b2fd1ea9
r21857jl <joe.loach@student.manchester.ac.uk>
2022-11-24 11:20:46 +0000

    Complete implementation of ShovelRemoveTest.
    The test was initially a stub method, always failing.
    It's now been updated to actually test the proper functionality
    of the Shovel's removeSoil function.

--- tests/games/stendhal/server/entity/item/ShovelTest.java
+++ tests/games/stendhal/server/entity/item/ShovelTest.java
@@ -1,23 +1,35 @@
 package games.stendhal.server.entity.item;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.maps.MockStendlRPWorld;
 
 
 public class ShovelTest {
     @BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		MockStendlRPWorld.get();
-	}
+    public static void setUpBeforeClass() throws Exception {
+        MockStendlRPWorld.get();
+    }
 
     @Test 
     public void ShovelRemoveTest()
     {
-        fail();
+        final StendhalRPZone zone = new StendhalRPZone("zone");
+        Shovel s = new Shovel("shovel", "tool", "shovel", null);
+        // add soil to the world at 0,0
+        s.addSoil(zone, 0, 0);
+        // Soil should be able to be removed when there's soil already there.
+        assertTrue(s.removeSoil(zone, 0, 0));
+        // shouldn't be able to remove it a second time, because there should'nt be any soil
+        assertFalse(s.removeSoil(zone, 0, 0));
+        // removing a soil at a position that never had any soil should also fail
+        assertFalse(s.removeSoil(zone, 1, 1));
     }
 
     @Test
