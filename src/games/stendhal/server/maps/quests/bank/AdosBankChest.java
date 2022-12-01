341bda5e61016946fb6e4a01491752073a618fd0
Felix Waller <felix.waller@student.manchester.ac.uk>
2022-12-01 11:17:59 +0000

    Add stub code for testing

---
+++ src/games/stendhal/server/maps/quests/bank/AdosBankChest.java
@@ -0,0 +1,34 @@
+package games.stendhal.server.maps.quests.bank;
+
+import java.util.List;
+
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.maps.quests.AbstractQuest;
+
+public class AdosBankChest extends AbstractQuest {
+
+  @Override
+  public List<String> getHistory(Player player) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public String getSlotName() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void addToWorld() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public String getName() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  
+}
